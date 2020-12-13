ce4a312f64f4dc17a767db299199b1ce49c21071
elenabaulina <elena@baulina.ru>
2020-12-13 22:23:28 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+poema = open("my_poema.txt", "w")
+n = 1
+while True:
+    msg = "Введите " + str(n) + " строку поэмы: (для выхода нажмите два раза Enter)"
+    s = input(msg)
+    if s == '': break
+    n+=1
+    poema.write(s + '\n')
+poema.close
+f = open('stih.txt')
+line = 0
+for i in f:
+    line += 1
+
+    flag = 0
+    word = 0
+    for j in i:
+        if j != ' ' and flag == 0:
+            word += 1
+            flag = 1
+        elif j == ' ':
+            flag = 0
+
+    print(i, len(i), 'симв.', word, 'сл.')
+
+print(line, 'стр.')
+f.close()
